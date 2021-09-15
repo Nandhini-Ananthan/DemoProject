145d54884e6b93368ba39404a19295a9229b8d38
Nandhini-A <nandhini.a1901@gmail.com>
2021-09-16 00:10:45 +0530

    Added display method in Class1 and called in DAL

--- BusinessObjectLayer/Class1.cs
+++ BusinessObjectLayer/Class1.cs
@@ -4,5 +4,11 @@ namespace BusinessObjectLayer
 {
     public class Class1
     {
+        public void display()
+        {
+            string name = "Nandhini";
+            string dept = "Development";
+            Console.WriteLine($"Name is {name} and Department {dept}");
+        }
     }
 }
---
+++ DataAccessLayer/DAL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BusinessObjectLayer;
+namespace DataAccessLayer
+{
+    public class DAL
+    {
+        public static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            obj.display();
+        }
+    }
+}
